f3e65faef1cf7c967eef4b7dff52f5abc1fd8cbb
neeraj-tosh <neeraj@toshblocks.com>
2018-12-01 15:46:00 +0530

    added socket.io and connect disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -2,13 +2,19 @@ const path = require('path');
 const express = require('express');
 const publicPath= path.join(__dirname,'../public');
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
+const http = require('http');
 
 const app = express();
 app.use(express.static(publicPath));
-// app.get(`${publicPath}/index`,(req,res)=>{
-//     res.send('welcome');
-// })
-
-app.listen(port,()=>{
+var server = http.createServer(app);
+var io = socketIO(server);
+io.on('connection',(socket)=>{
+    console.log('New User Connected');
+    socket.on('disconnect',()=>{
+        console.log('User disconnected');
+    })
+})
+server.listen(port,()=>{
     console.log(`Server is up and running on port ${port}`);
 })
